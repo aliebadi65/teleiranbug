d33a58847abef1aaff44869dd7036d19c8d11d4e
aliebadi65 <aliebadi66@gmail.com>
2016-05-13 20:56:41 +0430

    Update leave.lua

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -1,20 +1,21 @@
-do local function run(msg, matches) 
-local user_bot = 205573990 
+do
+-- Will leave the group if be added
+local function run(msg, matches)
+local bot_id = 205573990 
 local receiver = get_receiver(msg)
-    if matches[1] == 'sgergwr' and is_sudo(msg) then
-       channel_kick_user("channel#id"..msg.to.id, 'user#id'..user_bot, ok_cb, false)
-       chat_del_user("chat#id"..msg.to.id, 'user#id'..user_bot, ok_cb, false)
-    leave(receiver, ok_cb, false)
-    elseif msg.service and msg.action.type == 'chat_add_user' or 'channel_invite_user' and msg.action.user.id == tonumber(user_bot) and not is_sudo(msg) then
-       send_large_msg(receiver, '', ok_cb, false)
-       chat_del_user(receiver, 'user#id'..user_bot, ok_cb, false)
-       channel_kick_user("channel#id"..msg.to.id, 'user#id'..user_bot, ok_cb, 
-false)
-    leave(receiver, ok_cb, false)
-    end end
+    if matches[1] == 'leave' and is_admin1(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+	   leave_channel(receiver, ok_cb, false)
+    elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
+       send_large_msg(receiver, 'This is not one of my groups.', ok_cb, false)
+       chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
+	   leave_channel(receiver, ok_cb, false)
+    end
+end
  
 return {
   patterns = {
+    "^[#!/](leave)$",
     "^!!tgservice (.+)$",
   },
   run = run
